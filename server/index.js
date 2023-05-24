135aa891b1d5a2e26b03c3ebffa6a20ac8767522
Your_Name <Your-Email>
2023-05-24 16:12:52 +0530

    first day

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const dotenv=require('dotenv')
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
+const dotenv = require('dotenv')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 app.use(express.json());
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 require('./db/conn');
-const User=require('./model/UserSchema');
+const User = require('./model/UserSchema');
 app.use(require('./router/auth'));
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
     console.log(`lisenting ${PORT}`)
 })
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,26 +1,53 @@
-const express=require('express');
-const router= express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 require('./../db/conn');
-const User=require('./../model/UserSchema');
-router.get('/home',(req,res)=>{
-    res.send('hiii')
-})
-router.post('/register',(req,res)=>{
-      const {name,email,phone,work,password,cpassword}=req.body;
-      if(!name  || !email  || !phone  || !work  || !password  || !cpassword){
-         res.status(422).json({"err":"érr"})
-      }else{
-        User.findOne({email}).then((res)=>{
-          
-           if(!res){
-            const user=new User({name,email,phone,work,password,cpassword})
-            user.save().then((res)=>{
-                console.log(res)
-            }).catch((err)=>{
-                console.log(err)
-            })
-           }
+const User = require('./../model/UserSchema');
+
+router.post('/register', (req, res) => {
+    const { name, email, phone, work, password, cpassword } = req.body;
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        res.status(422).json({ "message": "All fields are required" })
+    } else {
+        User.findOne({ email }).then((userExist) => {
+
+            if (!userExist) {
+                const user = new User({ name, email, phone, work, password, cpassword })
+                user.save().then(() => {
+                    return res.status(200).json({ "message": "success" })
+                }).catch((err) => {
+                    return res.status(400).json({ "message": "database error" })
+                })
+            } else {
+                return res.status(400).json({ "message": "already exist" })
+            }
         })
-      }
+    }
 })
-module.exports=router;
+
+// loginroute
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(500).json({ "message": "please enter your email and password" })
+    } else {
+        const findUser = await User.findOne({ email });
+        if (!findUser) {
+            res.status(500).json({ "message": "please enter valid email address" })
+        } else {
+            const compare = await bcrypt.compare(password, findUser.password);
+            if (compare) {
+                res.status(200).json({ "message": "user log in successfully" });
+            } else {
+                res.status(500).json({ "message": "please enter valid email and password" });
+            }
+        }
+    }
+
+})
+
+
+
+module.exports = router;
